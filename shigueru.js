7e84b6048bed9d38fe57e950255bcb8358faba60
shigueru nagata <shigueru7191@gmail.com>
2015-01-03 16:08:27 -0500

    agragar carpeta

--- shigueru.js
+++ shigueru.js
@@ -2,7 +2,7 @@ document.oncontextmenu = function() {
         return false
         }
 function right(e) {
-        var msg = "Prohibido usar click Derecho !!! ";
+        var msg = "No puedes usar click Derecho !!! ";
         if (navigator.appName == 'Netscape' && e.which == 3) {
                 alert(msg) ;
                 return flase;
